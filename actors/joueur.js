4af57f597fecb4ae047a80f81f11fa15396ce181
Lanagramme <lborromee16@gmail.com>
2022-02-23 01:29:35 -0400

    code du joueur

--- actors/joueur.js
+++ actors/joueur.js
@@ -1,5 +1,33 @@
-class Arbitre{
+import * from ./deck.js
 
+let liste_des_cartes = [1,2,3,4]
+
+class Joueur = {
+	constructor (pv) {
+		this.decks = decks(liste_des_cartes)
+		this.init_pv = pv
+		this.pv = pv
+	}
+
+	set pv(pv) {
+		// l'arbitre décidera si il est possible d'avoir plus de pv que de init_pv
+		this.pv+= pv
+		// l'arbitre définit le statut de défaite
+	}
+
+	piocher(){
+		return this.decks.move(this.decks.Library, this.library.length,this.decks.Hand)
+	}
+
+	attaquer(cible, attaquant){}
+
+	sacrifier(origin, target){
+		return this.decks.move(origin, target, this.decks.Graveyard)
+	}
+
+	jouer(carte){
+		return this.decks.move(this.decks.Hand, carte, this.decks.Field)
+	}
 }
 
-module.exports = Arbitre
+module.exports = Joueur 
